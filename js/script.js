1e7e10592080c3793fc06035ff99fd9c95bd8004
Nir Geier <nirgeier@gmail.com>
2019-02-27 01:03:24 +0200

    Updates

--- js/script.js
+++ js/script.js
@@ -2,4 +2,17 @@
   $('.digit').on("webkitAnimationEnd", function (e) {
     this.classList.add('digit-color');
   });
+
+  $('.counter-count').each(function () {
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 5000,
+      easing: 'swing',
+      step: function (now) {
+        $(this).text(Math.ceil(now));
+      }
+    });
+  });
+
 })();
