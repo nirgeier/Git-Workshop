2ecef1c34a91f8a11a74cc0cffe84eb29c59cb0d
Nir Geier <nirgeier@gmail.com>
2019-03-04 01:50:29 +0200

    Added CNAME

--- js/script.js
+++ js/script.js
@@ -3,16 +3,40 @@
     this.classList.add('digit-color');
   });
 
-  $('.counter-count').each(function () {
-    $(this).prop('Counter', 0).animate({
-      Counter: $(this).text()
-    }, {
-        duration: 5000,
-        easing: 'swing',
-        step: function (now) {
-          $(this).text(Math.ceil(now));
-        }
-      });
+  /**
+   * Animate counters
+   */
+  // $('.counter').each(function () {
+  //   $(this).prop('Counter', 0)
+  //     .animate({
+  //       Counter: $(this).text()
+  //     }, {
+  //       duration: 5000,
+  //       easing: 'swing',
+  //       step: function (now) {
+  //         $(this).text(Math.ceil(now).toLocaleString());
+  //       }
+  //     });
+  // });
+
+
+  /**
+   * Get the form data
+   */
+  $(".contactForm").submit(function (event) {
+    event.preventDefault();
+
+    $.ajax({
+      "url": "https://docs.google.com/forms/d/e/1FAIpQLSf_4jvAw7Y8LaL5Qf_gMfMbxd8KsZo9CyJWE5nzQv2OpO5STw/formResponse",
+      "method": "POST",
+      data: $(this).serialize(),
+      done: function (data) {
+        alert('form submitted');
+      }
+    });
+
+
   });
+
 
 })();
