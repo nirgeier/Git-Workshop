fe58657701d644c3081c47ae8b646a6241fcf1d1
Nir Geier <nirgeier@gmail.com>
2019-03-06 16:31:24 +0200

    CSS Updates

--- js/script.js
+++ js/script.js
@@ -39,12 +39,7 @@
 
   //jQuery to collapse the navbar on scroll
   $(window).scroll(function () {
-    if ($("nav").offset().top > 50) {
-      $("nav").addClass("top-nav-collapse");
-    } else {
-      $("nav").removeClass("top-nav-collapse");
-    }
-
+    $(".navbar-nav").removeClass("show");
   });
 
 })();
